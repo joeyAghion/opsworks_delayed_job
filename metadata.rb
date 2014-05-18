b7bbd3aea5c1083f93fdcb2d1788dacbcfe523e5
Joey Aghion <joey@aghion.com>
2014-05-18 00:41:12 -0400

    Bump ot version 0.5

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Artsy"
 maintainer_email "it@artsymail.com"
 license          "MIT"
 description      "Configure and deploy background job workers."
-version          "0.4"
+version          "0.5"
 
 recipe 'opsworks_delayed_job::setup', 'Set up delayed_job worker.'
 recipe 'opsworks_delayed_job::configure', 'Configure delayed_job worker.'
