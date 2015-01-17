16bb3239aff9a2f25ac790f99fa6ec24812a29e1
Douglas Camata <d.camata@gmail.com>
2015-01-17 18:58:42 -0200

    changing default delayed_job bin path (script folder has been dropped along with rails 2)

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,19 +11,19 @@ else
 end
 
 node[:deploy].each do |application, deploy|
-  
+
   default[:delayed_job][application] = {}
-  default[:delayed_job][application][:path_to_script] = node[:delayed_job][:path_to_script] || 'script'
+  default[:delayed_job][application][:path_to_script] = node[:delayed_job][:path_to_script] || 'bin'
   default[:delayed_job][application][:pools] = {}
-  
+
   # Default to node[:delayed_job][:pool_size] workers, each with empty ({}) config.
   default[:delayed_job][application][:pools][:default] = Hash[node[:delayed_job][:pool_size].times.map{|i| [i.to_s, {}] }]
-  
+
   # Use node[:delayed_job][application][:pools][HOSTNAME] if provided.
   default[:delayed_job][application][:pool] = node[:delayed_job][application][:pools][node[:opsworks][:instance][:hostname]] || node[:delayed_job][application][:pools][:default]
   Chef::Log.debug("Set delayed_job attributes for #{application} to #{node[:delayed_job][application].to_hash.inspect}")
-  
+
   default[:delayed_job][application][:restart_command] = "sudo monit restart -g delayed_job_#{application}_group"
-  
+
 end
 
